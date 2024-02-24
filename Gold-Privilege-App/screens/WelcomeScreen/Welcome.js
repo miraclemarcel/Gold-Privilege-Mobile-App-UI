292478eaaab7ced3a8006d25cc4f5b6a3af89fbd
miraclemarcel <miraclemarcel29@gmail.com>
2024-02-24 19:58:56 +0100

    update

--- Gold-Privilege-App/screens/WelcomeScreen/Welcome.js
+++ Gold-Privilege-App/screens/WelcomeScreen/Welcome.js
@@ -8,10 +8,6 @@ import Slide from './Slides';
 import 
 { 
   Colors,
-  OnboardingTextContainer,
-  OnboardingTextDesc,
-  OnboardingTextTitle
-
 
 } from '../../styles/Style';
 
@@ -38,30 +34,21 @@ const WelcomeSlides = [
 },
 ]
 
-export const Slide = ({item}) => {
-  return (
-    <View style={{alignItems: 'center'}}>
-      <Image source={item.welcomeImage} style={{height: 250, resizeMode: 'contain', width}}/>
-      <OnboardingTextContainer>
-        <OnboardingTextTitle>{item.title}</OnboardingTextTitle>
-        <OnboardingTextDesc>{item.description}</OnboardingTextDesc>
-      </OnboardingTextContainer>
-
-    </View>
-  );
-};
 
 
 const Welcome = () => {
   return (
     <SafeAreaView style={ {flex: 1}}>
       <StatusBar backgroundColor={Colors.primary}/>
-            <FlatList 
-            data={WelcomeSlides} 
-            // contentContainerStyle={{ height: height * 0.75}} 
-            showsHorizontalScrollIndicator={false}
-            renderItem={({item}) => <Slide item={item} />}
-            />
+
+          <FlatList
+            data={WelcomeSlides}
+              // contentContainerStyle={{ height: height * 0.75}} 
+              showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => <Slide item={item} />}
+            keyExtractor={item => item.id.toString()}
+          />
+     
     </SafeAreaView>
   )
 }
